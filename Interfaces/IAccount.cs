5eb54cd2b01ba54cc039aadff16431cb066eef36
Hajar Ashour <hajrr320466@gmail.com>
2025-12-17 20:46:25 +0200

    Add files via upload

---
+++ Interfaces/IAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem.Interfaces
+{
+    /*Defines the essential structure and behavior that every bank account must follow. 
+     *It ensures that all account types share the same basic properties (AccountId, CustomerId, Balance), 
+     *and implement the core operations (Deposit and Withdraw).
+     *To provide a unified contract for all account types and enable polymorphism in the system.*/
+    public interface IAccount
+    {
+        #region Properties
+        int AccountId { get; }
+        int CustomerId { get; }
+        decimal Balance { get; }
+        #endregion
+
+        #region Methods
+        void Deposit(decimal amount);
+        void Withdraw(decimal amount); 
+        #endregion
+    
+    }
+}
---
+++ Interfaces/ITransaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem.Interfaces
+{
+    /*Represents the required structure of any financial transaction in the system. 
+     *It defines the basic information that every transaction must contain, such as ID, account, amount, date, and type.
+     *To ensure consistency in how transactions are recorded and represented.*/
+    public interface ITransaction
+    {
+        #region Properties
+        int TransactionId { get; }
+        int AccountId { get; }
+        decimal Amount { get; }
+        DateTime Date { get; }
+        string Type { get; } 
+        #endregion
+    }
+}
